bd0f3237001b15f3009d98dbb236ec0c029a45ec
tm-sakuma <tm-sakuma@pocket-queries.co.jp>
2025-10-02 17:44:04 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,28 @@ export const metadata: Metadata = {
     shortcut: "/favicon.ico",   // ショートカット用
     apple: "/favicon.png",      // iOS・Androidホーム画面用
   },
+  openGraph: {
+    title: "株式会社DAISIO - 健康診断システム",
+    description: "医療機関向けクラウド健診システム",
+    url: "https://www.daysio.co.jp/",
+    siteName: "株式会社DAISIO",
+    images: [
+      {
+        url: "/ogp.png", // public/ に配置したOGP画像
+        width: 1200,
+        height: 630,
+        alt: "株式会社DAISIO",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "株式会社DAISIO - 健康診断システム",
+    description: "医療機関向けクラウド健診システム",
+    images: ["/ogp.png"],
+  },
 }
 
 export default function RootLayout({
